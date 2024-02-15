2cadbbe7bba1f8cd60ffc01327e3eb9bd8221f6b
TayLee <thlee991@gmail.com>
2024-02-15 16:45:10 +0900

    nav custom-component

--- package/js/main.js
+++ package/js/main.js
@@ -4,3 +4,21 @@ const menu =  document.querySelector('.menu_ul');
 toggleBtn.addEventListener('click', ()=>{
     menu.classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownButton = document.getElementById('dropdownButton');
+    var dropdownContent = document.getElementById('dropdownContent');
+
+    dropdownButton.addEventListener('click', function() {
+        dropdownContent.classList.toggle('show');
+    });
+
+    // 클릭 이외의 영역을 클릭하면 드롭다운이 닫히도록 설정
+    window.addEventListener('click', function(event) {
+    if (!event.target.matches('#dropdownButton')) {
+            if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+            }
+        }
+    });
+}); 
---
+++ package/js/nav.js
@@ -0,0 +1,51 @@
+class nav_class extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =
+        '<nav class="navigator">' +
+        '<h3 class="site-name"><a href="TAYLEE Home.html">TAYLEE\'s <a class="name-archive">Archive</a></a></h3>' +
+        '<ul class="menu_ul">' +
+            '<li class="list_menu"><a id="dropdownButton" class="dropbtn" onclick="">Main</a>' +
+                '<div id="dropdownContent" class="dropdown-content">' +
+                    '<a class="nav_menu" href="TAYLEE Home.html">Archive</a>' +
+                    '<a class="nav_menu" href="index.html">Page</a>' +
+                '</div></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="TAYLEE Info.html">Info</a></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="TAYLEE documents.html">Documents</a></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="TAYLEE research.html">Research</a></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="TAYLEE project.html">Project</a></li>' +
+        '</ul>' +
+        '<a href="#" class="nav__toogle">' +
+        '<i class="hamburger"><img id="ham_menu" alt="hamburger" src="./resources/main/hamburger menu.png" width=24 height=24></i>' +
+        '</a>' +
+        '</nav>';        
+    }
+}
+
+customElements.define('custom-nav', nav_class);
+
+
+//project nav 경로가 달라서 어쩔수 없이 따로 해줘야 한다.
+class other_nav_class extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =
+        '<nav class="navigator">' +
+        '<h3 class="site-name"><a href="TAYLEE Home.html">TAYLEE\'s <a class="name-archive">Archive</a></a></h3>' +
+        '<ul class="menu_ul">' +
+            '<li class="list_menu"><a id="dropdownButton" class="dropbtn" onclick="">Main</a>' +
+                '<div id="dropdownContent" class="dropdown-content">' +
+                    '<a class="nav_menu" href="../TAYLEE Home.html">Archive</a>' +
+                    '<a class="nav_menu" href="../index.html">Page</a>' +
+                '</div></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="../TAYLEE Info.html">Info</a></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="../TAYLEE documents.html">Documents</a></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="../TAYLEE research.html">Research</a></li>' +
+            '<li class="list_menu"><a class="nav_menu" href="../TAYLEE project.html">Project</a></li>' +
+        '</ul>' +
+        '<a href="#" class="nav__toogle">' +
+        '<i class="hamburger"><img id="ham_menu" alt="hamburger" src="./resources/main/hamburger menu.png" width=24 height=24></i>' +
+        '</a>' +
+        '</nav>';        
+    }
+}
+
+customElements.define('other-custom-nav', other_nav_class);
