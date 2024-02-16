d6733acfcf21ec2d594da0263690b425fe10ce11
TayLee <thlee991@gmail.com>
2024-02-16 23:08:01 +0900

    update)

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -9,3 +9,22 @@ function mouseleaveHandler() {
 function goPage(){
     document.querySelector('.container2').scrollIntoView({behavior:'smooth'});
 }
+
+//index portfolio 등록
+document.addEventListener('DOMContentLoaded', function() {
+    var arrow_page = document.getElementById('arrow-page');
+    var hoverBox = document.getElementById('imgHoverEvent');
+
+    arrow_page.addEventListener('click', function() {
+        hoverBox.classList.toggle('show');
+    });
+
+    // 클릭 이외의 영역을 클릭하면 드롭다운이 닫히도록 설정
+    window.addEventListener('click', function(event) {
+    if (!event.target.matches('#arrow-page')) {
+            if (hoverBox.classList.contains('show')) {
+                hoverBox.classList.remove('show');
+            }
+        }
+    });
+}); 
