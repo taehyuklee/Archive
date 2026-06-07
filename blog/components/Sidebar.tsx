13c9581712c1fea6dd5e2aba4dd371ea48a85e1b
TayLee <thlee991@gmail.com>
2026-06-07 19:43:09 +0900

    commit) fix svg

--- blog/components/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import WidgetSlot from './WidgetSlot';
-
-export default function Sidebar() {
-  return (
-    <aside className="text-sm">
-      <WidgetSlot name="sidebar" />
-    </aside>
-  );
-}
